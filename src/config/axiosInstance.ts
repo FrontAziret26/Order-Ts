057727b775d9af7a49c2404f41489892fb7aa2c9
ITAziret26 <azirettoichubekov263@gmail.com>
2023-06-20 17:29:07 +0600

    TypeScript finish OrderFood

---
+++ src/config/axiosInstance.ts
@@ -0,0 +1,32 @@
+import axios from 'axios'
+import { store } from '../store'
+import { logOut } from '../store/auth/auth.thunk'
+
+const BASE_URL =
+  'http://ec2-18-197-107-37.eu-central-1.compute.amazonaws.com:5500/api/v1'
+
+export const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+})
+
+axiosInstance.interceptors.request.use(
+  function (config) {
+    config.headers.set('Authorization', store.getState().auth.token)
+    return config
+  },
+  function (error) {
+    return error
+  }
+)
+
+axiosInstance.interceptors.response.use(
+  function (config) {
+    return config
+  },
+  function (error) {
+    if (error.response.status === 401) {
+      store.dispatch(logOut())
+    }
+    return error
+  }
+)
